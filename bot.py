4796ade5d648c4028e2cdcda30bc33fa172a48c7
MouleFrites <33952878+MouleFrites@users.noreply.github.com>
2019-10-22 16:14:27 +0200

    update_docker

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-#import os
 import json
 import tweepy
+import os
 
 class MyStreamListener(tweepy.StreamListener):
     def __init__(self, api):
@@ -10,16 +10,22 @@ class MyStreamListener(tweepy.StreamListener):
         self.me = api.me()
 
     def on_status(self, tweet):
-        print(f"{tweet.user.name}:{tweet.text}")
-        if tweet.user.name != "Crypteur":
-            self.api.update_status(f"@{tweet.user.screen_name} On dit chiffrer, et pas crypter :)", tweet.id)
+        print(tweet.text)
+        
+        if 'décrypter' in tweet.text.lower() or 'Décrypter' in tweet.text.lower():
+            print('pas celui la !')
+        else:
+            print(f"{tweet.user.screen_name}:{tweet.text}")
+            if (tweet.user.screen_name != "crypteur") and (not tweet.retweeted) and ('RT @' not in tweet.text):
+                self.api.update_status(f"@{tweet.user.screen_name} On dit chiffrer, et pas crypter :)", tweet.id)
 
     def on_error(self, status):
         print("Error detected")
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("", "")
-auth.set_access_token("", "")
+
+auth = tweepy.OAuthHandler(os.environ['consumer_api'], os.environ['consumer_api_secret'])
+auth.set_access_token(os.environ['acces_token'], os.environ['acces_token_secret'])
 
 # Create API object
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
@@ -27,4 +33,5 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 tweets_listener = MyStreamListener(api)
 stream = tweepy.Stream(api.auth, tweets_listener)
-stream.filter(track=["crypter", "cryptez", "crypté", "cryptée", "cryptés", "cryptées"], languages=["fr"])
+stream.filter(track=["crypter", "cryptez", "crypté", "cryptée", "cryptés", "cryptées", "cryptage"], languages=["fr"])
+
