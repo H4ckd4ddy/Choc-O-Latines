ca49b731462ea9753a749bfa7e61471e1aec769f
Etienne Sellan <git@sellan.fr>
2019-10-22 10:33:55 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+#import os
+import json
+import tweepy
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+        print(f"{tweet.user.name}:{tweet.text}")
+        if tweet.user.name != "Crypteur":
+            self.api.update_status(f"@{tweet.user.screen_name} On dit chiffrer, et pas crypter :)", tweet.id)
+
+    def on_error(self, status):
+        print("Error detected")
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("", "")
+auth.set_access_token("", "")
+
+# Create API object
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+
+tweets_listener = MyStreamListener(api)
+stream = tweepy.Stream(api.auth, tweets_listener)
+stream.filter(track=["crypter", "cryptez", "crypté", "cryptée", "cryptés", "cryptées"], languages=["fr"])
